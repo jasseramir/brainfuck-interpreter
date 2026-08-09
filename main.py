d227e19f5ea9377790317b5f8bd41286694360a9
jasseramir <jasser.amir.work@gmail.com>
2026-08-09 04:55:05 +0300

    implement brainfuck instructions except loop instructions

--- main.py
+++ main.py
@@ -12,17 +12,28 @@ def compile(code):
     while i < len(clean):
         match clean[i]:
             case ">":
-                
+                tape.append(0)
+                pointer += 1
+
             case "<":
-                
+                pointer -= 1
+
             case "+":
-                
+                tape[pointer] = (tape[pointer] + 1) % 256
+
             case "-":
-                
+                tape[pointer] = (tape[pointer] - 1 + 256) % 256
+
             case ".":
-                
+                output += chr(tape[pointer])
+
             case ",":
-                
+                char = input("Enter a character:")
+
+                char = char[0]
+
+                tape[pointer] = ord(char)
+
             case "[":
                 
             case "]":
